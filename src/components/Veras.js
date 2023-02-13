81d98a2ad3e44ce0d6566a184c2f719595616c46
ivanedrei <ivanramirez2014@gmail.com>
2023-02-12 19:54:11 -0600

    react init

---
+++ src/components/Veras.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Veras.css"
+
+export const Veras = () => (
+    <>
+        <h2>Nashville Veras</h2>
+        <small>Loving care when you're not there.</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+    </>
+)
