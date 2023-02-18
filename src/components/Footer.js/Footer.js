3436dff2af38929689f0057ffc27adb51608c11d
ivanedrei <ivanramirez2014@gmail.com>
2023-02-17 21:37:40 -0600

    navbar and footer created

--- src/components/Footer.js/Footer.js
+++ src/components/Footer.js/Footer.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './Footer.css'
 
 export const Footer = () => {
     return <>
-        <h6>GET STARTED TODAY</h6>
-        <h2>Ready To Transform Your Home
-            Into Your Dream Home?</h2>
-        <button>Schedule consultation</button>
-        <div>
-            <h1>FOLLOW US</h1>
-            <a>Facebook</a>
-            <a>Instagram</a>
-            <a>Linkedin</a>
+        <div className="flexy">
+            <div className="footers footer1">
+                <h6 className="footer_">GET STARTED TODAY</h6>
+                <h1>Ready To Transform Your Home</h1>
+                <h1> Into Your Dream Home?</h1>
+            </div>
+            <div className="footers">
+                <button className="footer_btn">Schedule Consultation</button>
+            </div>
         </div>
-        <div>
+        <div className="black">
+            <div className="footers footer2">
+                <div className="flexer">
+                    <h1>FOLLOW US</h1>
+                </div>
+                <div className="flexer">
+                    <div className="footer_space">
+                        <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Facebook_colored_svg_copy-512.png"
+                            alt="facebook" className="footer_icon" />
+                        <Link to={"https://www.facebook.com/people/Veras-Custom-Cabinets/100063657056941/"} className="footer_font" >Facebook </Link>
+                    </div>
+                    <div className="footer_space">
+                        <img src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_instagram-512.png"
+                            alt="facebook" className="footer_icon" />
+                        <Link to={"https://www.instagram.com/veras.custom/"} className="footer_font">Instagram</Link>
+                    </div>
+                </div>
+                <div className="flexer">
+                    <h6>Copyright © All rights reserved.</h6>
+                </div>
+            </div>
+        </div>
+        {/* <div>
             <h6>Copyright © All rights reserved.</h6>
-        </div>
+        </div> */}
     </>
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,14 @@ import "./Navbar.css"
 export const Navbar = () => {
     return <>
         <div className="Nav_1">
-            <Link to="/" > Home </Link >
-            <Link to="/gallery" > gallery </Link >
-            <Link to="/about" > about us </Link >
-            <Link to="/contact" > contact </Link >
+
+            <img src="https://static.vecteezy.com/system/resources/previews/013/707/220/original/abstract-initial-letter-gw-or-wg-logo-in-gold-color-isolated-in-black-background-applied-for-carpentry-business-logo-also-suitable-for-the-brands-or-companies-have-initial-name-wg-or-gw-vector.jpg"
+                alt="logo" className="logo" />
+
+            <Link to="/" className="links"> Home </Link >
+            <Link to="/gallery" className="links"> Gallery </Link >
+            <Link to="/about" className="links"> About Us </Link >
+            <Link to="/contact" className="links"> Contact </Link >
         </div>
     </>
 }
